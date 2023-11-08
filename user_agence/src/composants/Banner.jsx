7e88c1273ccfb0aa50b9c7f1556867df0acf54a6
Robert Matundu <matundukabamba@gmail.com>
2023-11-08 12:54:20 +0100

    update servie and mission

--- user_agence/src/composants/Banner.jsx
+++ user_agence/src/composants/Banner.jsx
@@ -6,23 +6,28 @@ function Banner() {
     <>
       <div className=" md:px-14 px-4 max-w-screen-2xl mx-auto my-12">
         <div className=" md:p-9 px-4 py-9 bg-violet-500 rounded-tl-[100px] rounded-br-[100px]">
-          <motion.div
-            variants={fadeIn("up", 0.2)}
-            initial="hidden"
-            whileInView={"show"}
-            viewport={{ once: false, amount: 0.7 }}
-            className=" flex flex-col md:flex-row-reverse justify-between items-center gap-10"
-          >
+          <div className=" flex flex-col md:flex-row-reverse justify-between items-center gap-10">
             {/* image */}
-            <div>
+            <motion.div
+              variants={fadeIn("down", 0.2)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+            >
               <img
                 className=" lg:h-full w-full rounded-br-full rounded-tr-full"
                 src="../../public/Portraithome.png"
                 alt="a propos"
               />
-            </div>
+            </motion.div>
             {/* La présentation */}
-            <div className=" md:w-full">
+            <motion.div
+              variants={fadeIn("down", 0.3)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className=" md:w-full"
+            >
               <h2 className=" md:text-4xl text-3xl font-bold text-white mb-6 leading-relaxed">
                 Je suis le premier de ce bannier, en répresentant Artha beauté.
               </h2>
@@ -37,8 +42,8 @@ function Banner() {
                   contactez-nous
                 </button>
               </div>
-            </div>
-          </motion.div>
+            </motion.div>
+          </div>
         </div>
       </div>
     </>
